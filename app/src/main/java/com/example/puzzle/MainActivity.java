d28652631c42fa0beb8be4a943634ede6129a50e
Bogdan <burcea.bogdan.madalin@gmail.com>
2019-04-21 16:13:59 +0300

    added Log Tag in MainActivity

--- app/src/main/java/com/example/puzzle/MainActivity.java
+++ app/src/main/java/com/example/puzzle/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.puzzle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    public static String COMMON_TAG = "puzzletag";
+    public static String TAG = MainActivity.COMMON_TAG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TAG += this.getClass().getSimpleName();
+
+        Log.i(TAG, "test");
     }
 }
