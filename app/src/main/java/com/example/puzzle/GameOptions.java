6a2dd8c7e6a2883b7ad9b3a83284dc2e617f37e4
Anca-Nicoleta <anca.sotir98@gmail.com>
2019-04-22 01:18:09 +0300

    Working Game Options Activity

--- app/src/main/java/com/example/puzzle/GameOptions.java
+++ app/src/main/java/com/example/puzzle/GameOptions.java
@@ -1,22 +1,125 @@
 package com.example.puzzle;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class GameOptions extends AppCompatActivity {
-
-
+    private EditText rowInput;
+    private EditText columnInput;
+    private String imageSelected = null;
+    private Integer imageSelectedId = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_options);
+
+        rowInput = findViewById(R.id.inputRowNumber);
+        columnInput = findViewById(R.id.inputColumnNumber);
     }
 
     public void startGame(View view) {
         Intent intent = new Intent(this, SquareGame.class);
+
+        String rowsEntered = rowInput.getText().toString();
+        if(rowsEntered.equals("")){
+            Toast toast = Toast.makeText(getApplicationContext(), "No Row Number Entered!", Toast.LENGTH_LONG);
+            toast.show();
+            return;
+        }
+        String columnsEntered = columnInput.getText().toString();
+        if(columnsEntered.equals("")){
+            Toast toast = Toast.makeText(getApplicationContext(), "No Column Number Entered!", Toast.LENGTH_LONG);
+            toast.show();
+            return;
+        }
+
+        int rowNumber = Integer.parseInt(rowsEntered);
+        int columnNumber = Integer.parseInt(columnsEntered);
+
+        boolean incorrectFlag = false;
+        String message = null;
+
+        if(rowNumber<2){
+            incorrectFlag = true;
+            message = "Row Number Too Small!";
+        }
+        if(!incorrectFlag && rowNumber>9){
+            incorrectFlag = true;
+            message = "Row Number Too Big!";
+        }
+        if(!incorrectFlag && columnNumber<2){
+            incorrectFlag = true;
+            message = "Column Number Too Small!";
+        }
+        if(!incorrectFlag && columnNumber>10){
+            incorrectFlag = true;
+            message = "Column Number Too Big!";
+        }
+
+        if(!incorrectFlag && imageSelected==null){
+            incorrectFlag = true;
+            message = "No Image Selected!";
+        }
+
+        if(incorrectFlag){
+            Toast toast = Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG);
+            toast.show();
+            return;
+        }
+
+        intent.putExtra("rowNumber", rowNumber);
+        intent.putExtra("columnNumber", columnNumber);
+        intent.putExtra("imageSelected", imageSelected);
         startActivity(intent);
+
+        //in SquareGame Activity: String value = getIntent().getExtras().getString(key);
+    }
+
+    private void applySelectedImageTheme (View view){
+        ImageView image = (ImageView)view;
+
+        String name = "@color/colorAccent";
+        int id = getResources().getIdentifier(name, "drawable", getPackageName());
+        Drawable drawable = getResources().getDrawable(id);
+
+        int sdk = android.os.Build.VERSION.SDK_INT;
+        if(sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+            image.setBackgroundDrawable(drawable);
+        } else {
+            image.setBackground(drawable);
+        }
+
+        if(imageSelected != null){
+            ImageView currentSelectedImage = findViewById(imageSelectedId);
+            removeSelectedImageTheme(currentSelectedImage);
+        }
+    }
+
+    private void removeSelectedImageTheme (ImageView imageView){
+        imageView.setBackgroundColor(0x00000000);
+    }
+
+    public void selectImage(View view) {
+        if(view.getId()==R.id.imageView1){
+            applySelectedImageTheme(view);
+
+            imageSelected = "@drawable/p1";
+            imageSelectedId = R.id.imageView1;
+            return;
+        }
+        if(view.getId()==R.id.imageView2){
+            applySelectedImageTheme(view);
+            imageSelected = "@drawable/p2";
+            imageSelectedId = R.id.imageView2;
+            return;
+        }
     }
 }
